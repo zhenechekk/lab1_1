a09b98f4de0ef99dfb2f1c454afa53949bf3d46e
Evgenia Malinova <malinova.jenya@gmail.com>
2023-10-04 12:25:15 +0300

    ласт

--- js/shop.js
+++ js/shop.js
@@ -87,7 +87,7 @@ $(document).ready(function() {
 			<input readonly class="product0"></input>
 			<input readonly class="amount0"></input>
 			</div>`)
-			$('.product0').val("ИСПРАВИТЬ");
+			$('.product0').val("Букет Радость весны");
 			$('.amount0').val(cart[i]);
 					break;
 
@@ -99,7 +99,7 @@ $(document).ready(function() {
 			<input readonly class="product1"></input>
 			<input readonly class="amount1"></input>
 			</div>`)
-			$('.product1').val("Product2");
+			$('.product1').val("Букет Летняя нежность");
 			$('.amount1').val(cart[i]);
 					break;
 		
@@ -111,7 +111,7 @@ $(document).ready(function() {
 			<input readonly class="product2"></input>
 			<input readonly class="amount2"></input>
 			</div>`)
-			$('.product2').val("Product3");
+			$('.product2').val("Букет Осенний романтизм");
 			$('.amount2').val(cart[i]);
 					break;
 			
@@ -123,7 +123,7 @@ $(document).ready(function() {
 			<input readonly class="product3"></input>
 			<input readonly class="amount3"></input>
 			</div>`)
-			$('.product3').val("Product4");
+			$('.product3').val("Букет Зимняя сказка");
 			$('.amount3').val(cart[i]);
 					break;
 			
@@ -135,12 +135,96 @@ $(document).ready(function() {
 			<input readonly class="product4"></input>
 			<input readonly class="amount4"></input>
 			</div>`)
-			$('.product4').val("Product5");
+			$('.product4').val("Букет Радуга чувств");
 			$('.amount4').val(cart[i]);
 					break;
 
 				default:
 		
+				case 5:
+					//var t = "Product6";
+					document.querySelector('.cart-body').insertAdjacentHTML(
+			'afterbegin',
+			`<div class="row">
+			<input readonly class="product5"></input>
+			<input readonly class="amount5"></input>
+			</div>`)
+			$('.product4').val("Букет Цветочный танец");
+			$('.amount4').val(cart[i]);
+					break;
+
+				case 6:
+					//var t = "Product7";
+					document.querySelector('.cart-body').insertAdjacentHTML(
+			'afterbegin',
+			`<div class="row">
+			<input readonly class="product6"></input>
+			<input readonly class="amount6"></input>
+			</div>`)
+			$('.product4').val("Букет Розовый рассвет");
+			$('.amount4').val(cart[i]);
+					break;
+
+				case 7:
+					//var t = "Product8";
+					document.querySelector('.cart-body').insertAdjacentHTML(
+			'afterbegin',
+			`<div class="row">
+			<input readonly class="product7"></input>
+			<input readonly class="amount7"></input>
+			</div>`)
+			$('.product4').val("Букет Белая элегия");
+			$('.amount4').val(cart[i]);
+					break;
+
+				case 8:
+					//var t = "Product9";
+					document.querySelector('.cart-body').insertAdjacentHTML(
+			'afterbegin',
+			`<div class="row">
+			<input readonly class="product8"></input>
+			<input readonly class="amount8"></input>
+			</div>`)
+			$('.product4').val("Букет Синий океан");
+			$('.amount4').val(cart[i]);
+					break;
+
+				case 9:
+					//var t = "Product10";
+					document.querySelector('.cart-body').insertAdjacentHTML(
+			'afterbegin',
+			`<div class="row">
+			<input readonly class="product9"></input>
+			<input readonly class="amount9"></input>
+			</div>`)
+			$('.product4').val("Букет Золотая осень");
+			$('.amount4').val(cart[i]);
+					break;
+
+				case 10:
+					//var t = "Product11";
+					document.querySelector('.cart-body').insertAdjacentHTML(
+			'afterbegin',
+			`<div class="row">
+			<input readonly class="product10"></input>
+			<input readonly class="amount10"></input>
+			</div>`)
+			$('.product4').val("Букет Фиолетовый вихрь");
+			$('.amount4').val(cart[i]);
+					break;
+
+				case 11:
+					//var t = "Product12";
+					document.querySelector('.cart-body').insertAdjacentHTML(
+			'afterbegin',
+			`<div class="row">
+			<input readonly class="product11"></input>
+			<input readonly class="amount11"></input>
+			</div>`)
+			$('.product4').val("Букет Красный пыл");
+			$('.amount4').val(cart[i]);
+					break;
+
 				break;
 			}
 		//$('.product').val(t);
